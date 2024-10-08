31182142bb1dec9fb2c7d3e9edff13d88c120ad9
Jorge Casas <150157169+jorgecasase@users.noreply.github.com>
2024-10-08 18:00:41 +0200

    Add files via upload

---
+++ com/game/player/decorator/GalardonComponent.java
@@ -0,0 +1,7 @@
+package com.game.player.decorator;
+
+public interface GalardonComponent {
+    public String getDescripcion();
+    public Integer getDanio();
+    public Integer getNumGalardones();
+}
---
+++ com/game/player/decorator/GalardonComponentDecorator.java
@@ -0,0 +1,5 @@
+package com.game.player.decorator;
+
+public interface GalardonComponentDecorator extends GalardonComponent{
+    public GalardonComponent getGalardonComponent();
+}
